846081901c65b128e6e5b1c5aa54ae97755d924c
SardorBoboyev <boboyevsardor350@gmail.com>
2024-09-27 11:05:38 +0500

    Initial commit

---
+++ src/test/java/uz/sb/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sb.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
